286ea501d0f2623d3490dd560d0d2d98482c24da
wraithio <aarmon5@gmail.com>
2025-03-28 00:20:48 -0700

    added resume

--- src/App.jsx
+++ src/App.jsx
@@ -13,22 +13,34 @@ function App() {
     <div className="min-h-screen text-white font-[Raleway]">
       <NavbarComponent />
       {/* splash */}
-        <div className="flex justify-center gap-2 mt-24 mb-4">
+        <div className="flex justify-center gap-2 mt-24 animate-fade">
           <img
             src="/wave.webp"
             alt="wave emoji"
             className="w-10 animate-wiggle"
           />
-          <h3 className="text-3xl">Hi there.</h3>
+          <h3 className="text-3xl">Hi there, I'm Aaron.</h3>
         </div>
-      <div className="flex text-center flex-col gap-2 mb-24">
+      <div className="flex text-center flex-col gap-2 mb-24 animate-fade">
 
-        <h1 className="text-6xl">I help people's dreams become reality</h1>
+        <h1 className="text-6xl font-[Cormorant] my-4">I create art with my mouse and keyboard</h1>
+        <h3 className="text-3xl">fullstack web developer: versed in both backend and frontend development</h3>
         {/* <CarouselComponent/> */}
       </div>
-      <div className="bg-white bg-opacity-20 justify-self-center w-[80%] animate-fade3">
-        <div className="flex justify-center backdrop-blur-sm  p-2">
-          Something here
+      <div className="bg-white bg-opacity-20 filter justify-self-center w-[80%]">
+        <div className="flex justify-center p-2">
+          <div className="flex gap-1">
+            <h3 className="text-xl">
+              view my resume
+            </h3>
+            <a href="https://tr.ee/C9SrLCD8f7" className="cursor-pointer w-fit" target="_blank">
+
+            <h3 className="text-xl text-emerald-200">
+              here
+            </h3>
+            </a>
+
+          </div>
         </div>
       </div>
     </div>
