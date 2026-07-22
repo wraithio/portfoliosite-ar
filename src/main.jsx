50e76fdff362d0ab0c731cad66b7f7c73c642abd
wraithio <aarmon5@gmail.com>
2026-07-22 01:05:56 -0700

    add vercel analytics

--- src/main.jsx
+++ src/main.jsx
@@ -3,10 +3,12 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import CursorComponent from './components/CursorComponent.jsx'
+import { Analytics } from '@vercel/analytics/react'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
     {/* <CursorComponent/> */}
+    <Analytics/>
   </StrictMode>,
 )
